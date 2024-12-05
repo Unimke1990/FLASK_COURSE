cf9a49ff257a6cdbae2459062b40e9760e036eab
Unimke1990 <agimunimke1990@gmail.com>
2024-12-05 13:16:30 +0200

    flash messgaes

--- first_app/app5.py
+++ first_app/app5.py
@@ -1,6 +1,6 @@
-#session and cookies management, and message flashing
+#session & cookies management, and message flashing
 
-from flask import Flask, render_template, session, make_response
+from flask import Flask, render_template, session, make_response, request
 
 app5 = Flask(__name__, template_folder='templates')
 app5.secret_key = 'given'
@@ -33,12 +33,30 @@ def clear_session():
     session.clear()
     return render_template('page5.html', message=f"session cleared")
 
-
+#creating cookies which are saved on the browsers
 @app5.route('/create_cookie')
 def create_cookie():
     response = make_response(render_template('page5.html', message='cookie set'))
     response.set_cookie(key='cookie_name', value='cookie_value')
     return response
+
+
+#get cookie information
+@app5.route('/get_cookie')
+def get_cookie():
+    cookie_value = request.cookies.get('cookie_name')
+    return render_template('page5.html', message=f"cookie value: {cookie_value}")
+
+
+# Remove cookie
+@app5.route('/remove_cookie')
+def remove_cookie():
+    response = make_response(render_template('page5.html', message='cookie removed'))
+    response.set_cookie('cookie_name', expires=0)
+    return response
+
+
+
 
 
 
